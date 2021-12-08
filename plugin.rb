a124bd43e7acc18d68a46b0ebad66515406cff1d
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2021-12-08 23:59:46 +0100

    init

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: basic-plugin
+# about: A super simple plugin to demonstrate how plugins work
+# version: 0.0.1
+# authors: Awesome Plugin Developer
+
+enabled_site_setting :awesomeness_enabled
