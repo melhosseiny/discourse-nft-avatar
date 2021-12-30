82ee74f1ac69d601a8f7b9127aa365c31954b5a6
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2021-12-30 11:37:20 +0100

    3 column grid layout on mobile

--- assets/javascripts/discourse/initializers/extend-avatar-selector.js
+++ assets/javascripts/discourse/initializers/extend-avatar-selector.js
@@ -6,58 +6,9 @@ import autoGroupFlairForUser from "discourse/lib/avatar-flair";
 import I18n from "I18n";
 
 export default {
-  name: "init",
+  name: "extend-avatar-selector",
   initialize() {
     withPluginApi("1.0.0", api => {
-      api.includePostAttributes("user_nft_verified");
-
-      api.customUserAvatarClasses(user => {
-        return user.custom_fields.nft_verified ? ["nft"] : [];
-      });
-
-      api.addTopicParticipantClassesCallback((attrs) => {
-        return attrs.user_nft_verified ? ["nft"] : [];
-      });
-
-      api.reopenWidget("post-avatar", {
-        html(attrs) {
-          let body;
-          if (!attrs.user_id) {
-            body = iconNode("far-trash-alt", { class: "deleted-user-avatar" });
-          } else {
-            body = avatarFor.call(this, this.settings.size, {
-              template: attrs.avatar_template,
-              username: attrs.username,
-              name: attrs.name,
-              url: attrs.usernameUrl,
-              className: "main-avatar",
-              hideTitle: true,
-              extraClasses: attrs.user_nft_verified ? "nft" : ""
-            });
-          }
-
-          const postAvatarBody = [body];
-
-          if (attrs.flair_url || attrs.flair_bg_color) {
-            postAvatarBody.push(this.attach("avatar-flair", attrs));
-          } else {
-            const autoFlairAttrs = autoGroupFlairForUser(this.site, attrs);
-
-            if (autoFlairAttrs) {
-              postAvatarBody.push(this.attach("avatar-flair", autoFlairAttrs));
-            }
-          }
-
-          const result = [h("div.post-avatar", postAvatarBody)];
-
-          if (this.settings.displayPosterName) {
-            result.push(this.attach("post-avatar-user-info", attrs));
-          }
-
-          return result;
-        }
-      })
-
       api.modifyClass("controller:avatar-selector", {
         pluginId: "discourse-nft-avatar",
         uploader: "",
---
+++ assets/javascripts/discourse/initializers/nft-avatar-class.js
@@ -0,0 +1,62 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import { h } from "virtual-dom";
+import { avatarFor } from "discourse/widgets/post";
+import autoGroupFlairForUser from "discourse/lib/avatar-flair";
+import I18n from "I18n";
+
+export default {
+  name: "nft-avatar-class",
+  initialize() {
+    withPluginApi("1.0.0", api => {
+      api.includePostAttributes("user_nft_verified");
+
+      api.customUserAvatarClasses(
+        user => user.custom_fields.nft_verified ? ["nft"] : []
+      );
+
+      api.addTopicParticipantClassesCallback(
+        attrs => attrs.user_nft_verified ? ["nft"] : []
+      );
+
+      api.reopenWidget("post-avatar", {
+        html(attrs) {
+          let body;
+          if (!attrs.user_id) {
+            body = iconNode("far-trash-alt", { class: "deleted-user-avatar" });
+          } else {
+            body = avatarFor.call(this, this.settings.size, {
+              template: attrs.avatar_template,
+              username: attrs.username,
+              name: attrs.name,
+              url: attrs.usernameUrl,
+              className: "main-avatar",
+              hideTitle: true,
+              extraClasses: attrs.user_nft_verified ? "nft" : ""
+            });
+          }
+
+          const postAvatarBody = [body];
+
+          if (attrs.flair_url || attrs.flair_bg_color) {
+            postAvatarBody.push(this.attach("avatar-flair", attrs));
+          } else {
+            const autoFlairAttrs = autoGroupFlairForUser(this.site, attrs);
+
+            if (autoFlairAttrs) {
+              postAvatarBody.push(this.attach("avatar-flair", autoFlairAttrs));
+            }
+          }
+
+          const result = [h("div.post-avatar", postAvatarBody)];
+
+          if (this.settings.displayPosterName) {
+            result.push(this.attach("post-avatar-user-info", attrs));
+          }
+
+          return result;
+        }
+      })
+    });
+  }
+}
