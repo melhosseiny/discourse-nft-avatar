c6f93cc30e59d1a0f49d5b5489679dc63024ef91
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2022-01-01 21:56:18 +0100

    remove debug statements from verify-nft-owner

--- assets/javascripts/discourse/initializers/verify-nft-owner.js
+++ assets/javascripts/discourse/initializers/verify-nft-owner.js
@@ -19,14 +19,10 @@ export default {
         nft_contract_address,
         nft_token_id,
       } = currentUser.custom_fields;
-      // console.log("nft_custom_fields", currentUser.custom_fields);
 
       if (nft_verified === true) {
         const owner = await owner_of(nft_token_id, nft_contract_address);
         const ownerAddress = web3.eth.abi.decodeParameter("address", owner);
-        // const address = "0xdccac502461c0d8261daf2ab3e411663e39b2654";
-        // console.log(nft_wallet_address, nft_wallet_address.length, ownerAddress, ownerAddress.length);
-        // console.log(nft_wallet_address.toLowerCase() === ownerAddress.toLowerCase());
         if (nft_wallet_address.toLowerCase() !== ownerAddress.toLowerCase()) {
           currentUser.set("custom_fields.nft_verified", false);
           currentUser.set("user_option", {});
